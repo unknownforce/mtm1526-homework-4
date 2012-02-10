6535528cb02735844cf118e374aad54f7bcbcb4d
Petrus Chan <petchan34@gmail.com>
2012-02-10 01:39:05 -0500

    updated

--- js/tabtest.js
+++ js/tabtest.js
@@ -4,11 +4,16 @@ $(document).ready(function() {
 	
 	$('.tab-buttons a').on('click', function (ev) {
 		var tabToShow = $(this).attr('href');
+		
+		var current = $('#tab-group .current').index();
 		console.log(tabToShow);
+	
+		$(tabToShow).addClass('current');
+		if (tabToShow == true){
+			$(this).removeClass('current');
+		}
 		
-		$(tabToShow).addClass('current'); // $('#tab-1')
+		//}); 
 			
-		$(this).removeClass('current');
-		
 	});
 });
