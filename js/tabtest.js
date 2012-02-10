19719973ebf275b5065092c0cd2c7f07a6c05849
unknownforce <petchan34@gmail.com>
2012-02-10 10:22:36 -0500

    current fixed

--- js/tabtest.js
+++ js/tabtest.js
@@ -4,14 +4,12 @@ $(document).ready(function() {
 	
 	$('.tab-buttons a').on('click', function (ev) {
 		var tabToShow = $(this).attr('href');
-		
+		$('#tab-group .current').removeClass('current');
 		var current = $('#tab-group .current').index();
 		console.log(tabToShow);
 	
 		$(tabToShow).addClass('current');
-		if (tabToShow == true){
-			$(this).removeClass('current');
-		}
+		
 		
 		//}); 
 			
